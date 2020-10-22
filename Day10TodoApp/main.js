304221b7b3064bd90eccd77345bd9233b641e8e9
Hazelle2511 <62952538+Hazelle2511@users.noreply.github.com>
2020-10-22 20:21:27 +0200

    Add files via upload

---
+++ Day10TodoApp/main.js
@@ -0,0 +1,20 @@
+let ourForm = document.getElementById("ourForm");
+let ourField = document.getElementById("ourField");
+let ourList = document.getElementById("ourList");
+ourForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //console.log(ourField.value)
+  createItem(ourField.value);
+});
+
+function createItem(x) {
+  let ourHTML = `<li>${x} <button onclick = "deleteItem(this)">Delete</button></li>`;
+  ourList.insertAdjacentHTML("beforeend", ourHTML);
+  ourField.value = "";
+  ourField.focus();
+}
+
+function deleteItem(elementToDelete) {
+  //alert("Delete requested")
+  elementToDelete.parentElement.remove();
+}
